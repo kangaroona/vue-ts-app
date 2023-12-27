f3b767d8162bef262a370a58b63f83770f93b8a6
kangaroona <79397550@qq.com>
2023-12-26 23:35:26 -0800

    feat: init vue3-ts

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { viteMockServe } from "vite-plugin-mock";
+import path from "path";
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    viteMockServe({
+      mockPath: "mock",
+      // localEnabled: command === "serve",
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@components": path.resolve(__dirname, "./src/components"),
+    },
+  },
+});
